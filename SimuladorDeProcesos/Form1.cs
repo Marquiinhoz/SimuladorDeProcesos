38a2a00348589a03849b1b599c224f430aee021a
5haeq <hectoryampufebv@gmail.com>
2025-11-23 22:27:17 -0500

    feat: owo

--- SimuladorDeProcesos/Form1.cs
+++ SimuladorDeProcesos/Form1.cs
@@ -24,6 +24,135 @@ namespace SimuladorDeProcesos
         public Form1()
         {
             InitializeComponent();
+            ApplyCatppuccinMochaTheme();
+        }
+
+        private void ApplyCatppuccinMochaTheme()
+        {
+            // Paleta Catppuccin Mocha (valores hex)
+            var rosewater = ColorTranslator.FromHtml("#F5E0DC");
+            var flamingo  = ColorTranslator.FromHtml("#F2CDCD");
+            var pink      = ColorTranslator.FromHtml("#F5C2E7");
+            var mauve     = ColorTranslator.FromHtml("#CBA6F7");
+            var peach     = ColorTranslator.FromHtml("#FAB387");
+            var yellow    = ColorTranslator.FromHtml("#F9E2AF");
+            var green     = ColorTranslator.FromHtml("#A6E3A1");
+            var teal      = ColorTranslator.FromHtml("#94E2D5");
+            var sky       = ColorTranslator.FromHtml("#89DCEB");
+            var blue      = ColorTranslator.FromHtml("#89B4FA");
+            var text      = ColorTranslator.FromHtml("#CAD3F5");
+            var surface0  = ColorTranslator.FromHtml("#363A4F");
+            var surface1  = ColorTranslator.FromHtml("#2A2E3E");
+            var base0     = ColorTranslator.FromHtml("#1E1E2E");
+
+            // Color fijo para subtítulos: blanco
+            var subtitleColor = ColorTranslator.FromHtml("#FFFFFF");
+
+            // Fondo y texto global: usar base0 como fondo principal
+            this.BackColor = base0;
+            this.ForeColor = text;
+
+            // Recorrer y aplicar estilos según control
+            foreach (var ctrl in GetAllControls(this))
+            {
+                switch (ctrl)
+                {
+                    case Button b:
+                        b.FlatStyle = FlatStyle.Flat;
+                        b.BackColor = surface1;
+                        b.ForeColor = text;
+                        b.FlatAppearance.BorderColor = mauve;
+                        b.FlatAppearance.BorderSize = 1;
+                        break;
+
+                    case Label l:
+                        // Detectar si es subtítulo: Tag="subtitle" o nombre que contiene title/subtitle o fuente en negrita
+                        var name = (l.Name ?? "").ToLowerInvariant();
+                        bool isSubtitle = (l.Tag != null && l.Tag.ToString().ToLowerInvariant() == "subtitle")
+                                          || name.Contains("title")
+                                          || name.Contains("subtitle")
+                                          || l.Font.Bold;
+
+                        if (isSubtitle)
+                        {
+                            // Subtítulos forzados a blanco y en negrita
+                            l.ForeColor = subtitleColor;
+                            l.Font = new Font(l.Font.FontFamily, Math.Max(9f, l.Font.Size), FontStyle.Bold);
+                            l.BackColor = Color.Transparent;
+                        }
+                        else
+                        {
+                            l.ForeColor = text;
+                            l.BackColor = Color.Transparent;
+                        }
+                        break;
+
+                    case GroupBox gb:
+                        // Títulos de área (GroupBox) en blanco
+                        gb.ForeColor = subtitleColor;
+                        gb.BackColor = base0;
+                        break;
+
+                    case TabPage tp:
+                        tp.BackColor = base0;
+                        tp.ForeColor = subtitleColor;
+                        break;
+
+                    case Panel p:
+                        // paneles usan base0 para integrarse mejor con el fondo
+                        p.BackColor = base0;
+                        break;
+
+                    case ListView lv:
+                        lv.BackColor = surface1;
+                        lv.ForeColor = text;
+                        lv.BorderStyle = BorderStyle.None;
+                        break;
+
+                    case DataGridView dg:
+                        dg.BackgroundColor = surface1;
+                        dg.DefaultCellStyle.BackColor = surface1;
+                        dg.DefaultCellStyle.ForeColor = text;
+                        dg.ColumnHeadersDefaultCellStyle.BackColor = surface0;
+                        dg.ColumnHeadersDefaultCellStyle.ForeColor = text;
+                        dg.EnableHeadersVisualStyles = false;
+                        break;
+
+                    case TextBox tb:
+                        tb.BackColor = surface1;
+                        tb.ForeColor = text;
+                        tb.BorderStyle = BorderStyle.FixedSingle;
+                        break;
+
+                    case MenuStrip ms:
+                        ms.BackColor = surface0;
+                        ms.ForeColor = text;
+                        break;
+
+                    case ToolStrip ts:
+                        ts.BackColor = surface0;
+                        ts.ForeColor = text;
+                        break;
+                }
+            }
+
+            // Botón primario (si existe) con color de acento
+            var primaryBtn = this.Controls.OfType<Button>().FirstOrDefault();
+            if (primaryBtn != null)
+            {
+                primaryBtn.BackColor = mauve;
+                primaryBtn.ForeColor = base0;
+            }
+        }
+
+        private IEnumerable<Control> GetAllControls(Control parent)
+        {
+            foreach (Control c in parent.Controls)
+            {
+                yield return c;
+                foreach (var child in GetAllControls(c))
+                    yield return child;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
